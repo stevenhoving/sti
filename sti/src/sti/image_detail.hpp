907ddb02773b2fa5dcfe19ae693dd3db3ec75f0d
Robin Degen <robindegen@gmail.com>
2017-06-17 12:48:41 +0200

    Ported Steven's changes from image_detail and did some cleanups.

--- sti/src/sti/image_detail.hpp
+++ sti/src/sti/image_detail.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
+#include <sti/utility.hpp>
+#include <sti/pixel_values.hpp>
 #include <cstdint>
-#include <type_traits>
 #include <memory>
 
 namespace sti
@@ -12,9 +13,12 @@ namespace detail
 template <typename pixel_type_t>
 static void normalize_image(const uint8_t *src, pixel_type_t *dst, const int width, const int height)
 {
-    const auto henk = static_cast<pixel_type_t>(1.0) / static_cast<pixel_type_t>(255.0);
+    constexpr auto min_value = pixel_values<pixel_type_t>::min_value() + 1;
+    constexpr auto max_value = pixel_values<pixel_type_t>::max_value();
+
+    const auto factor = min_value / max_value;
     for (auto i = 0; i < width * height; ++i)
-        dst[i] = src[i] * henk;
+        dst[i] = src[i] * factor;
 }
 
 template <>
@@ -32,19 +36,21 @@ static void denormalize_image(const T *src, uint8_t *dst, const int width, const
 template <>
 static void denormalize_image<float>(const float *src, uint8_t *dst, const int width, const int height)
 {
-    const auto henk = 255.f;
+    constexpr auto factor = 255.0f;
+    constexpr auto min_value = static_cast<float>(pixel_values<uint8_t>::min_value());
+    constexpr auto max_value = static_cast<float>(pixel_values<uint8_t>::max_value());
     for (auto i = 0; i < width * height; ++i)
-        // \todo fix missing clamp here
-        dst[i] = static_cast<uint8_t>(src[i] * henk);
+        dst[i] = static_cast<uint8_t>(utility::clamp<float>(src[i] * factor, min_value, max_value));
 }
 
 template <>
 static void denormalize_image<double>(const double *src, uint8_t *dst, const int width, const int height)
 {
-    const auto henk = 255.0;
+    constexpr auto factor = 255.0;
+    constexpr auto min_value = static_cast<float>(pixel_values<uint8_t>::min_value());
+    constexpr auto max_value = static_cast<float>(pixel_values<uint8_t>::max_value());
     for (auto i = 0; i < width * height; ++i)
-        // \todo fix missing clamp here
-        dst[i] = static_cast<uint8_t>(src[i] * henk);
+        dst[i] = static_cast<uint8_t>(sti::utility::clamp<double>(src[i] * factor, min_value, max_value));
 }
 
 } // namespace detail
